26a2d52e633984bc64f9c71456e3e31747c1d0a7
takayuki matsuda <t.matsuda1986@gmail.com>
2017-05-29 21:53:39 +0900

    no message

--- app/controllers/kintais_controller.rb
+++ app/controllers/kintais_controller.rb
@@ -1,18 +1,17 @@
 class KintaisController < ApplicationController
     require 'date'
 
+    def new
+        @kintai = Kintai.new
+    end
+
     def create
         kintaiFrom = DateTime.now.hour.to_s + DateTime.now.minute.to_s
 
         @kintai = Kintai.new(kintai_date: Date.today, kintai_from: kintaiFrom)
         @kintai.save
-    end
 
-    def show
-        kintaiFrom = DateTime.now.hour.to_s + DateTime.now.minute.to_s
-
-        @kintai = Kintai.new(kintai_date: Date.today, kintai_from: kintaiFrom)
-        @kintai.save
+        redirect_to "top/index"
     end
 
 end
