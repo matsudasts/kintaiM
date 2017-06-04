e3391a0da23da4e80215fafb8cd0ec0382d28324
takayuki matsuda <t.matsuda1986@gmail.com>
2017-06-04 09:14:23 +0900

    no message

--- app/models/kintai.rb
+++ app/models/kintai.rb
@@ -1,8 +1,8 @@
 class Kintai < ApplicationRecord
-    validate :check_kintai_from
+    validate :check_kintai_time
 
     private
-    def check_kintai_from       
+    def check_kintai_time       
         if !kintai_from.present?
             errors.add("時間は必須入力です。","")
         elsif kintai_from.length != 4
@@ -10,19 +10,20 @@ class Kintai < ApplicationRecord
         elsif !correct_time(kintai_from)
             errors.add("不正な時間です。","")
         elsif Kintai.exists?(kintai_date: kintai_date)
-            errors.add("その日の出勤時間は既に登録されています。","")
+            errors.add("指定された年月の出勤は既に登録されています。","")
         end
     end
 
     private
     def correct_time(hhmm)
-        begin
-            hh = hhmm[0..1].to_i
-            mm = hhmm[2..3].to_i
-            DateTime.new(1900,1,1,hh,mm,0)
-        rescue
+        if hhmm !~ /^[0-9]+$/
             return false
         end
+
+        if !(hhmm[0..1].to_i).between?(0, 23) || !(hhmm[2..3].to_i).between?(0, 59)
+            return false
+        end
+
         true
     end
 
